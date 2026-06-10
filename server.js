ddda29393fd596f2c171b8107b4ce7dd588b9550
amer200 <amermostafa@gmail.com>
2026-06-10 08:18:56 +0300

    commit

--- server.js
+++ server.js
@@ -28,5 +28,29 @@ app.use('/admin', require('./routes/adminRoutes'));
 // 2. Technician API Routes
 // app.use('/api/tech', require('./routes/api/techRoutes'));
 
+
+const seedAdmins = async() => {
+
+    const isAdmin = await Admin.findOne();
+
+    if (!isAdmin) {
+        // نمسح أي أدمن قديم عشان نبدأ من الصفر بجد
+        await Admin.deleteMany({});
+
+        const admins = [
+            { username: 'Admin1990', password: '123' },
+            { username: 'Mohammed', password: '123' },
+            { username: 'Admin1992', password: '123' }
+        ];
+
+        for (let a of admins) {
+            a.password = await bcrypt.hash(a.password, 12); // تشفير
+        }
+
+        await Admin.insertMany(admins);
+        console.log("تم إضافة الـ ٣ أدمن بنجاح! اقفل الملف ده خلاص.");
+        process.exit();
+    }
+};
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Hybrid Server running on port ${PORT}`.yellow.bold));
